b8d68bb2aaf5d62d5f6e76b467ca619430e837e8
Luca <48834642+IlNin@users.noreply.github.com>
2020-07-27 11:34:39 -0400

    Add files via upload

--- tester.c
+++ tester.c
@@ -4,27 +4,30 @@
 #include <unistd.h>
 #include <sys/syscall.h>
 
-static int nThreads = 12;
-static int locks[12] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+#define nThreads 12
+
+static int locks[nThreads];
 
 void *call_system_call(void * p_index) {
 	int i = *((int *) p_index);
 	printf("I'm process ID: %d and I'm invoking the catnap system call!\n", i);
-	syscall(134, locks[i]); // sys_ni_syscall has index 134
+	syscall(134, &locks[i], i); // sys_ni_syscall has index 134
 	locks[i+1] = 1; 
 	printf("I'm process ID: %d and it looks like everything worked out ok!\n", i);
 }
 
-int main(int argc, char** argv) {
 
+int main(int argc, char** argv) {
 	// Allocates memory for nThreads threads
     pthread_t *threads = malloc(sizeof(pthread_t)*nThreads);
-	int *p_index = malloc(sizeof(int*));
-	*p_index = 0;
-	pthread_create(&threads[0], NULL, call_system_call, (void*) p_index);
-	pthread_join(threads[0], NULL);
+	
+	// Populates the lock array
+	locks[0] = 1;
+	for (int i = 1; i < nThreads; i++) {
+		locks[i] = 0; 
+	}
 
-    /* Starts the threads and takes the begin time
+    // Starts the threads and takes the begin time
     for (int i = 0; i < nThreads; i++) {
 		int *p_index = malloc(sizeof(int*));
 		*p_index = i;
@@ -34,6 +37,6 @@ int main(int argc, char** argv) {
     // Waits until all threads are finished
     for (int i = 0; i < nThreads; i++) {
         pthread_join(threads[i], NULL);
-    } */
+    }
     return 0;
 }
